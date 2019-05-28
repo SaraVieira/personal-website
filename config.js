badc1d6f26e27eab8fde77b1b0f233e088e44ca9
Sara Vieira <hey@iamsaravieira.com>
2019-05-28 22:02:36 +0200

    fix things

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'Ramblings by Sara Vieira',
   subtitle: 'Talks about Javascript, Mental Health and Airports maybe',
   copyright: '© All rights reserved.',
-  postsPerPage: 4,
+  postsPerPage: 10,
   // googleAnalyticsId: 'UA-73379983-2',
   menu: [
     {
@@ -13,21 +13,17 @@ module.exports = {
     {
       label: 'About me',
       path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
     }
   ],
   author: {
     name: 'Sara Vieira',
-    photo: 'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_400x400.jpg',
+    photo: '/photo.jpg',
     bio: 'Developer at @CodeSandbox, idiot, self proclaimed meme and useless shit maker.',
     contacts: {
       email: 'hey@iamsaravieira.com',
       twitter: 'NikkitaFTW',
       github: 'SaraVieira',
-      rss: '#'
+      rss: 'rss.xml'
     }
   }
 };
